fa935062b8f39cd86fc77e83d6f741212f7bcb9b
IvanPasciucco <54645974+IvanPasciucco@users.noreply.github.com>
2025-12-12 16:55:27 -0300

    corrijo otra vez5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -33,4 +33,57 @@ suite('Functional Tests', function() {
         assert.equal(res.status, 200);
         assert.equal(res.body.stockData.stock, 'GOOG');
         assert.isNumber(res.body.stockData.likes);
-        assert.isAbove(res.body.stockData.
+        assert.isAbove(res.body.stockData.likes, 0);
+        
+        // Guardamos la cantidad actual de likes para el siguiente test
+        likesStock1 = res.body.stockData.likes;
+        done();
+      });
+  });
+
+  test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG', like: true })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        
+        // La clave: Los likes DEBEN ser iguales al test anterior (no se incrementan)
+        // porque la IP es la misma (el test runner local)
+        assert.equal(res.body.stockData.likes, likesStock1); 
+        done();
+      });
+  });
+
+  test('Viewing two stocks: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'] })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.equal(res.body.stockData.length, 2);
+        
+        // Verificamos que exista la propiedad rel_likes
+        assert.property(res.body.stockData[0], 'rel_likes');
+        assert.property(res.body.stockData[1], 'rel_likes');
+        done();
+      });
+  });
+
+  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'], like: true })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.equal(res.body.stockData.length, 2);
+        assert.property(res.body.stockData[0], 'rel_likes');
+        assert.property(res.body.stockData[1], 'rel_likes');
+        done();
+      });
+  });
+
+});
